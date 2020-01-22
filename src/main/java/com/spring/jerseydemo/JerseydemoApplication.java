bb17bce03c0bffc0942e3931ed213bb81be71470
Aditya Rathore <adityarathore35@gmail.com>
2020-01-22 15:17:25 +0530

    First Commit   + implemented (CRUD)

---
+++ src/main/java/com/spring/jerseydemo/JerseydemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jerseydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseydemoApplication.class, args);
+	}
+
+}
